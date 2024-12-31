7d926b9e48b69f51ac7414812358e1a87dd2fcd4
re4rk <37167652+re4rk@users.noreply.github.com>
2025-01-01 01:06:33 +0900

    Initialize Spring Boot project with Gradle setup
    Added core project structure, build configuration, and dependencies. Included Spring Boot main class, test class, and application properties. Gradle wrapper and relevant gitignore settings were also configured.

---
+++ src/test/java/com/ark/driverlicense/DriverLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ark.driverlicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverLicenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
