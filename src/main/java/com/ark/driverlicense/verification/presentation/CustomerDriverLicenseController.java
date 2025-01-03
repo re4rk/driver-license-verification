e8aa537781dfd4118032e16d58a8425b1843b344
re4rk <37167652+re4rk@users.noreply.github.com>
2025-01-03 14:42:46 +0900

    Organize imports and annotations in controller class.
    Updated import statements to follow conventions and reordered annotations for better readability. This change ensures clarity and consistency in the codebase.

--- src/main/java/com/ark/driverlicense/verification/presentation/CustomerDriverLicenseController.java
+++ src/main/java/com/ark/driverlicense/verification/presentation/CustomerDriverLicenseController.java
@@ -1,11 +1,14 @@
 package com.ark.driverlicense.verification.presentation;
 
-import com.ark.driverlicense.verification.application.dtos.DriverLicenseDto;
 import com.ark.driverlicense.verification.application.DriverLicenseService;
+import com.ark.driverlicense.verification.application.dtos.DriverLicenseDto;
 import com.ark.driverlicense.verification.presentation.dtos.VerifyLicenseRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/driver-licenses")
